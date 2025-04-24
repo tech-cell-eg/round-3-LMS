fa82f553309def42c814c51b5a842b980675171a
KaremMetrial <karim.metrial@gmail.com>
2025-04-24 22:26:16 +0200

    Add ApiResponse trait for standardized JSON responses; enhance exception handling in app bootstrap

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+
+trait ApiResponse
+{
+    protected function successResponse($data = null, $message = null, $code = 200)
+    {
+        return response()->json([
+            'success' => (bool) true,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    protected function errorResponse($message = null, $code = 400)
+    {
+        return response()->json([
+            'success' => (bool) false,
+            'message' => $message,
+            'data' => null,
+        ], $code);
+    }
+
+
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +20,63 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*') || $request->is('admin/*')) {
+                return response()->json([
+                    'success' => (bool) false,
+                    'message' => 'Page 404 Not Found',
+                    'data'    => $e->getMessage(),
+                ], 404);
+            }
+        });
+
+        $exceptions->render(function (ModelNotFoundException $e, Request $request) {
+            if ($request->is('api/*') || $request->is('admin/*')) {
+                return response()->json([
+                    'success' => (bool) false,
+                    'message' => 'Model 404 Not Found',
+                    'data'    => $e->getMessage(),
+                ], 404);
+            }
+        });
+
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request) {
+            if ($request->is('api/*') || $request->is('admin/*')) {
+                return response()->json([
+                    'success' => (bool) false,
+                    'message' => 'Method Not Allowed',
+                    'data'    => $e->getMessage(),
+                ], 405);
+            }
+        });
+
+        $exceptions->render(function (ValidationException $e, Request $request) {
+            if ($request->is('api/*') || $request->is('admin/*')) {
+                return response()->json([
+                    'success' => (bool) false,
+                    'message' => 'Validation Error',
+                    'data'    => $e->errors(),
+                ], 422);
+            }
+        });
+
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*') || $request->is('admin/*')) {
+                return response()->json([
+                    'success' => (bool) false,
+                    'message' => 'Unauthorized Access',
+                    'data'    => $e->getMessage(),
+                ], 401);
+            }
+        });
+
+        $exceptions->render(function (Throwable $e, Request $request) {
+            if ($request->is('api/*') || $request->is('admin/*')) {
+                return response()->json([
+                    'success' => (bool) false,
+                    'message' => 'Internal Server Error',
+                    'data'    => $e->getMessage(),
+                ], 500);
+            }
+        });
     })->create();
