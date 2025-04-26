43afb8a9369b1d8f57fb7c894bc2abb556d4bf70
Tarek Hesham <tarekghost6@gmail.com>
2025-04-26 22:22:07 +0300

    [Categories] - Register apiResource and make seeder for dummy data.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Client\Request;
 
@@ -13,3 +14,5 @@ Route::prefix('auth')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
     });
 });
+
+Route::apiResource('categories', CategoryController::class);
